f33af26f3038a31200a6be634e423c1e008f97b4
lorenlemarr-pvcc <145620285+lorenlemarr-pvcc@users.noreply.github.com>
2023-11-01 18:43:23 -0400

    Add files via upload

--- petvet.py
+++ petvet.py
@@ -30,13 +30,16 @@ PR_LEP = 21
 PR_LYME = 41
 PR_RAB = 25
 
+PR_LEUK = 35
+PR_FVR = 30
+
 PR_ALL = 0
 
 PR_CHEWS_SMALL = 9.99
 PR_CHEWS_MED = 11.99
 PR_CHEWS_LARGE = 13.99
 
-# Define global variables
+chews_cost = 0
 
 # Define program functions
 def main():
@@ -62,7 +65,7 @@ def get_user_data():
     global pet_name, pet_type, pet_weight
     pet_name = input("Pet name: ")
     pet_type = input("Is this pet a dog (D) or cat (C)?: ")
-    pet_weight = input("Weight of your pet (in pounds): ")
+    pet_weight = int(input("Weight of your pet (in pounds): "))
 
 # DOG functions
 def get_dog_data():
@@ -76,14 +79,16 @@ def get_dog_data():
     print("\t6. Rabies")
     print("\t7. Full Vaccine Package (includes all vaccines)")
     print("\t8. NONE")
-    pet_vax = int(input("\n* Enter the vaccine number: "))
+    pet_vax_type = int(input("\n* Enter the vaccine number: "))
 
     print("\nMonthly heartworm prevention medication is recommended for all dogs.")
     heart_yesno = input("Would you like to order monthly heartworm medication for " + pet_name + " (Y/N)?: ")
     if heart_yesno.upper() == "Y":
-        num_chews = input("How many heartworm chews would you like to order?: ")
+        num_chews = int(input("How many heartworm chews would you like to order?: "))
 
 def perform_dog_calculations():
+    global PR_ALL, vax_cost, vax_name, chews_cost, total
+    
     PR_ALL = PR_BORD + PR_DAPP + PR_FLU + PR_LEP + PR_LYME + PR_RAB
 
     if pet_vax_type == 1:
@@ -111,26 +116,92 @@ def perform_dog_calculations():
         vax_name = "Rabies"
 
     elif pet_vax_type == 7:
-        vax_cost = PR_ALL
-        vax_name = "Full Vaccine Package (includes all vaccines)"
+        vax_cost = PR_ALL*.85
+        vax_name = "Full Vaccine Package"
 
     elif pet_vax_type == 8:
         vax_cost = 0
         vax_name = "NONE"
 
-
+    ##### Heart worm chews #######
+    if num_chews != 0:
+        if pet_weight < 25:
+            chews_cost = num_chews*PR_CHEWS_SMALL
+        elif pet_weight >= 25 and pet_weight < 50:
+            chews_cost = num_chews*PR_CHEWS_MED
+        else:
+            chews_cost = num_chews*PR_CHEWS_LARGE
+    ###### Find Total ######
+    total = vax_cost + int(chews_cost)
 def display_dog_results():
-    print("DISPLAY DOGS")
+    moneyf = '8,.2f'
+    print('------------------------------')
+    print('****PETVET*****')
+    print('Costs for Pet health products')
+    print('------------------------------')
+    print(vax_name + str('\t\t$') + format(vax_cost, moneyf))
+    print('Heart Worm Chews\t\t$' + format(chews_cost, moneyf))
+    print('Total\t\t\t\t$' + format(total, moneyf))
+    print('------------------------------')
+    print(str(datetime.datetime.now()))
 
 # CAT functions
 def get_cat_data():
-    print("CAT DATA")
+    global pet_vax_type, num_chews
+    print("\n** Cat Vaccines:")
+    print("\t1. Leukemia")
+    print("\t2. Feline Viral Rhinotracheitis")
+    print("\t3. Rabies")
+    print("\t4. Full Vaccine Package (includes all vaccines)")
+    print("\t5. NONE")
+    pet_vax_type = int(input("\n* Enter the vaccine number: "))
+
+    print("\nMonthly heartworm prevention medication is recommended for all cats.")
+    heart_yesno = input("Would you like to order monthly heartworm medication for " + pet_name + " (Y/N)?: ")
+    if heart_yesno.upper() == "Y":
+        num_chews = int(input("How many heartworm chews would you like to order?: "))
+
 
 def perform_cat_calculations():
-    print("CAT CALCS")
+    global vax_cost, pet_vax_type, chews_cost, total, vax_name
+
+    PR_ALL = PR_LEUK + PR_FVR + PR_RAB
+    
+    if pet_vax_type == 1:
+        vax_cost = PR_LEUK
+        vax_name = "Leukemia"
+
+    if pet_vax_type == 2:
+        vax_cost = PR_FVR
+        vax_name = "Feline Viral Rhinotracheitis"
+
+    if pet_vax_type == 3:
+        vax_cost = PR_RAB
+        vax_name = "Rabies"
+
+    if pet_vax_type == 4:
+        vax_cost = PR_ALL*.90
+        vax_name = "Full Vaccine Package"
+
+    if pet_vax_type == 5:
+        vax_cost = 0
+        vax_name = "NONE"
+
+    chews_cost = 8*num_chews
+
+    total = vax_cost + int(chews_cost)
 
 def display_cat_results():
-    print("DISPLAY CATS")
+    moneyf = '8,.2f'
+    print('------------------------------')
+    print('****PETVET*****')
+    print('Costs for Pet health products')
+    print('------------------------------')
+    print(vax_name + str('\t\t$') + format(vax_cost, moneyf))
+    print('Heart Worm Chews\t\t$' + format(chews_cost, moneyf))
+    print('Total\t\t\t\t$' + format(total, moneyf))
+    print('------------------------------')
+    print(str(datetime.datetime.now()))
 
 # Call the main program to execute
 main()
